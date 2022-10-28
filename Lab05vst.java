437dcda30b8476d3a7b6f41c96d08b96fd9f9570
13195 <ryan.miller@vtsd.com>
2022-10-28 07:58:14 -0400

    Lab 05

--- Lab05vst.java
+++ Lab05vst.java
@@ -6,32 +6,47 @@ import java.awt.*;
 import java.applet.*;
 
 
-public class Lab05vst extends Applet
-{
-    public void paint(Graphics g)
-    {
+public class Lab05vst extends Applet {
+    public void paint(Graphics g) {
         int width = 980;
         int height = 630;
         int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
-        g.drawRect(10,10,width,height);
+        g.drawRect(10, 10, width, height);
 
-        for(int p = 0; p < 980; p+=10){
+        for (int p = 0; p < 965; p += 14){
+            g.drawLine(x1 + p, y1, x2, y2 - p);
 
-                g.drawLine(x1 + p, y1, x2, y2 - p);
+            y2 += 5;
+        }
 
+        for (int p = 0; p < 965; p += 14)
+        {
+            g.drawLine(x2-p,y2,x1,y1-p);
 
+            y2=640;
+            y1 += 5;
+        }
+
+        for (int p = 0; p < 965; p +=14) {
+            g.drawLine(y1-p,x1,y2,x2-p);
 
 
 
         }
 
-            }
-        }
 
 
+
+
+
+
+
+                               }
+
+                                     }
 
 
 
